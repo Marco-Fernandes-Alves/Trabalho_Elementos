e3493b9c76d6339db81a9a1fce7fe566037793e1
Marco-Fernandes-Alves <marco.fernandes.alves@ubi.pt>
2025-05-27 21:36:00 +0100

    Update integrate.py

--- Versão_marco/src/integrate.py
+++ Versão_marco/src/integrate.py
@@ -5,7 +5,7 @@ def integrate_data(dataframes, output_file):
     try:
         if not dataframes:
             return None
-            
+
         df_final = dataframes[0]
         for df in dataframes[1:]:
             df_final = pd.merge(
@@ -14,15 +14,21 @@ def integrate_data(dataframes, output_file):
                 on=['Ano', 'Território'],
                 how='outer'
             )
-        
-        # Preencher NaN e ordenar
+
         df_final = df_final.fillna(0)
+        colunas_para_remover = []
+
+        for col in df_final.columns:
+            if col not in ['Ano', 'Território']:
+                if (df_final[col] == 0).all() or df_final[col].isnull().all():
+                    colunas_para_remover.append(col)
+
+        df_final = df_final.drop(columns=colunas_para_remover)
         df_final = df_final.sort_values(['Ano', 'Território'])
-        
         os.makedirs(os.path.dirname(output_file), exist_ok=True)
         df_final.to_csv(output_file, index=False, encoding='utf-8')
         return df_final
-        
+
     except Exception as e:
         print(f'Erro na integração: {str(e)}')
         return None
